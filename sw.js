0df07d2ac42b13e689febe0f8f7c2bd9bf5a3025
luffy01984-png <luffy01984@gmail.com>
2026-02-04 22:27:43 +0100

    Update service worker cache version and code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'annuaire-v3';
+const CACHE_NAME = 'annuaire-v3.1.1';
 const ASSETS = [
     './',
     './index.html',
@@ -38,3 +38,4 @@ self.addEventListener('fetch', (event) => {
             .then((response) => response || fetch(event.request))
     );
 });
+
