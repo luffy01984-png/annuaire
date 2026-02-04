6a1ea43f700b8e2c98cf8e8b5912eb9913bbc711
luffy01984-png <luffy01984@gmail.com>
2026-02-04 22:36:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'annuaire-v3.1.1';
+const CACHE_NAME = 'annuaire-v4';
 const ASSETS = [
     './',
     './index.html',
@@ -38,4 +38,3 @@ self.addEventListener('fetch', (event) => {
             .then((response) => response || fetch(event.request))
     );
 });
-
